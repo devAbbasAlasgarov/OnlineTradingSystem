f773028476cc337148a5396395d1728105691221
Abbas Alasgarov <devabbasalasgarov@gmail.com>
2024-08-27 12:38:33 +0400

    Add project files.

---
+++ OnlineTradingSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineTradingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
